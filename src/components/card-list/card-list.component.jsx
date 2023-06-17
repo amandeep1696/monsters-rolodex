2b0f0df1ebe8ba205c1eaa02c7114ddc56f6e56a
Amandeep Singh <am418470@dal.ca>
2023-06-16 22:56:42 -0300

    Pass props to component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,15 @@ import { Component } from 'react';
 
 class CardList extends Component {
   render() {
-    return <div>Hello im the cardlist component</div>;
+    const { monsters } = this.props;
+
+    return (
+      <div>
+        {monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
   }
 }
 
