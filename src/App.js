5eb48ec23437c533659783a3c05a8104d1c5fc77
Amandeep Singh <am418470@dal.ca>
2023-06-16 22:41:46 -0300

    Add CardList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -54,13 +56,15 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+  render() {
+    return <div>Hello im the cardlist component</div>;
+  }
+}
+
+export default CardList;
